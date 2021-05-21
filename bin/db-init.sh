fc4bf75541e88ba8336f8a4e73ab6acb672d1d37
Philip Spelman <philip.spelman@vaporstream.com>
2021-05-20 23:05:25 -0400

    - added script to set up initial tables + some experiment tables

--- bin/db-init.sh
+++ bin/db-init.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+
+# set up the DB
+echo "Setting up the DB tables"
+psql postgres -h localhost -d postgres -f /workspace/db-setup/sql/cars_tabls.sql
+
 # run the export for env vars
 echo "Running the script to set env vars"
 # shellcheck disable=SC2039
